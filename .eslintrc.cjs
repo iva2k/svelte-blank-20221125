9bb47a0fd251f41d7f68631149c482c5f0e37699
Ilya <iva2k@yahoo.com>
2022-11-25 20:54:53 -0800

    Fix some issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,26 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/recommended',
+		'prettier'
+	],
+	plugins: ['svelte3', '@typescript-eslint', 'import'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
+		'import/resolver': {
+			typescript: {}
+		},
 		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: ['./tsconfig.json', './tsconfig.lint.json'],
+		tsconfigRootDir: './'
 	},
 	env: {
 		browser: true,
