096349844ec67f18b36eb8fb921f07bf493778d6
Ilya <iva2k@yahoo.com>
2022-11-28 07:31:11 -0800

    Update playwright use of scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,9 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173
+    command: 'pnpm run svelte:build:only && pnpm run svelte:preview', // TODO: (blocked by upstream) having "pnpm run svelte:build" calls "cap sync android", which hits a bug with no extension windows command file crashing in node --experimental-loader. Current workaround is to remove `cap` from playwright.
+    port: 4173,
+    timeout: 120 * 1000
   }
 };
 
