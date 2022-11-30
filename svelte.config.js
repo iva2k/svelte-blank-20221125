0194b5ec3bdbc9868fcc207d27a71cde5a44b9df
Ilya <iva2k@yahoo.com>
2022-11-29 16:19:32 -0800

    Move viteStaticCopy from svelte.config.js to vite.config.ts

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import netlify from '@sveltejs/adapter-netlify';
 import vercel from '@sveltejs/adapter-vercel';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import assets from './assets.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -32,18 +30,7 @@ const config = {
     alias: {
       // Place to add all aliases. Run 'svelte-kit sync' (or npm run postinstall) to update paths in '.svelte-kit/tsconfig.json'.
       // $components: resolve('./src/lib/components')
-    },
-
-    vite: () => ({
-      plugins: [
-        // copy is needed for vite to work in svelte:dev (especially under "tauri dev")
-        // All copy commands are duplicated in package.json:scripts.svelte:prebuild, for svelte:dev to work correctly.
-        viteStaticCopy({
-          targets: assets,
-          verbose: true
-        })
-      ]
-    })
+    }
   }
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import assets from './assets.js';
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+
+    // copy is needed for vite to work in svelte:dev (especially under "tauri dev")
+    // All copy commands are duplicated in package.json:scripts.svelte:prebuild, for svelte:dev to work correctly.
+    viteStaticCopy({
+      targets: assets,
+      verbose: true
+    })
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
