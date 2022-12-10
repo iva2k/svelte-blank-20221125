a09048d5727e1f45874bd95e28df90afb8e8f9e3
Ilya <iva2k@yahoo.com>
2022-12-09 19:34:01 -0800

    Forward SEO component

--- src/lib/components/seo/SEO.svelte
+++ src/lib/components/seo/SEO.svelte
@@ -116,12 +116,12 @@
 
 <svelte:head>
   <title>{pageTitle}</title>
-  <!--
   <meta name="description" content={metadescription} />
   <meta
     name="robots"
     content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
   />
+  <!--
   <link rel="canonical" href={url} /> -->
 </svelte:head>
 
