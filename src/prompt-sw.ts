7863967b90e8271746b4156dbddb427279ec5a8b
Ilya <iva2k@yahoo.com>
2022-12-11 15:04:04 -0800

    Add setDefaultHandler to prompt-sw.js and claims-sw.js * Improve offline routing

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -8,7 +8,9 @@ import {
   createHandlerBoundToURL,
   precacheAndRoute
 } from 'workbox-precaching';
-import { NavigationRoute, registerRoute } from 'workbox-routing';
+import { NavigationRoute, registerRoute, setDefaultHandler } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
+// import { offlineFallback } from 'workbox-recipes';
 
 declare let self: ServiceWorkerGlobalScope;
 
@@ -30,3 +32,9 @@ if (import.meta.env.DEV) allowlist = [/^\/$/];
 // to allow work offline
 const url = '/';
 registerRoute(new NavigationRoute(createHandlerBoundToURL(url), { allowlist }));
+
+// This allows navigaing to routes like /sverdle in offline:
+setDefaultHandler(new NetworkFirst({ networkTimeoutSeconds: 1 }));
+
+// Unfortunately, this does not intercept uncached routes, such as form submits.
+// offlineFallback({ pageFallback: '/amOffline' }); // Needs setDefaultHandler() to work
