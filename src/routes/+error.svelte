97227c40b87e93bd2254dee17f8c4a897a5efda0
Ilya <iva2k@yahoo.com>
2022-12-11 13:34:36 -0800

    Add +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  const { message } = $page.error || { message: 'Oops, $page.error is null' };
+  const title = `${$page.status}: ${message}`;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+{#if false}
+  <p>This is `src/routes/+errror.svelte` file.</p>
+{/if}
