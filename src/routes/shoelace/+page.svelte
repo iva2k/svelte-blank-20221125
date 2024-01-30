f1b51f822eb0ecb86f14a5b2cd152d30f0d0e2c7
IVA2K <iva2k@yahoo.com>
2024-01-29 18:54:04 -0800

    Fix button variants

--- src/routes/shoelace/+page.svelte
+++ src/routes/shoelace/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-  // ? import sl-button from '@shoelace...'
 </script>
 
-<sl-button>Button</sl-button>
+<sl-button variant="default">Default Button</sl-button>
 
-<sl-button type="primary">Primary</sl-button>
+<sl-button variant="primary">Primary</sl-button>
 
-<sl-button type="Secondary">Secondary</sl-button>
+<sl-button variant="success">Success</sl-button>
+
+<sl-button variant="neutral">Neutral</sl-button>
+
+<sl-button variant="warning">Warning</sl-button>
+
+<sl-button variant="danger">Danger</sl-button>
